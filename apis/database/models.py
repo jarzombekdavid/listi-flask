7ce3709b4f5521bb6c71271537fe0fc478aa9994
David Jarzombek <davidjarzombek@Davids-MacBook-Pro.local>
2020-10-10 01:20:12 -0400

    updates

--- apis/database/models.py
+++ apis/database/models.py
@@ -1,13 +1,23 @@
 from pynamodb.models import Model
+from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
 from pynamodb.attributes import UnicodeAttribute, ListAttribute, MapAttribute, JSONAttribute, NumberAttribute
 
 
 # hash key is an index
-# sort key is an aditional search param
+# range key is an aditional search param
 # can really only search based on those 2 things
 
+# need an index on the email user so its easy to search by email for login purposes
+class EmailIndex(GlobalSecondaryIndex):
+    class Meta:
+        read_capacity_units = 1
+        write_capacity_units = 1
+        # All attributes are projected
+        projection = AllProjection()
+    email = UnicodeAttribute(hash_key=True)
+
 class Comment(MapAttribute):
-    comment_id = NumberAttribute(sort_key=True)
+    comment_id = NumberAttribute(hash_key=True)
     source_person = UnicodeAttribute()
     comment = UnicodeAttribute()
 
@@ -18,8 +28,16 @@ class Item(MapAttribute):
     free_text = UnicodeAttribute()
     item_dicts = JSONAttribute()
 
+# create new basemodel to add to_dict for easy serialization
+class BaseModel(Model):
+    def to_dict(self):
+        rval = {}
+        for key in self.attribute_values:
+            rval[key] = self.__getattribute__(key)
+        return rval
 
-class ListModel(Model):
+
+class ListModel(BaseModel):
     class Meta:
         table_name = 'lists'
     list_id = NumberAttribute(hash_key=True)
@@ -29,14 +47,14 @@ class ListModel(Model):
     comments = ListAttribute(of=Comment)
 
 
-class UserModel(Model):
+class UserModel(BaseModel):
     class Meta:
         table_name = 'users'
-    username = UnicodeAttribute(hash_key=True)
-    email = UnicodeAttribute(sort_key=True)
+    user_id = NumberAttribute(hash_key=True)
+    email = UnicodeAttribute()
+    email_index = EmailIndex()
     password = UnicodeAttribute()
-    lists = ListAttribute()
-
+    lists = ListAttribute(default=[])
 
 
 if not ListModel.exists():
--- apis/user.py
+++ apis/user.py
@@ -8,35 +8,38 @@ import logging
 api = Namespace('user', description='list operations')
 
 
-@api.route('/')
+@api.route('')
 class Users(Resource):
 
     def get(self):
         if request.args.get('email'):
-            UserModel.query(UserModel.email = request.args.get('email'))
+            UserModel.email_index(request.args.get('email'))
+        else:
+            return {'require email'}
+
 
     def post(self):
-        user_id = UserModel.count() + 1
+        user_id = UserModel.count() + 2
         params = request.args
         new_usr = UserModel(
-            user_id=user_id,
+            user_id,
             email=params['email'],
             password=params['password']
         ).save()
         # serialize new_usr
-        return new_usr
+        return user_id
 
 
 @api.route('/<user_id>')
-class SingleList(Resource):
+class SingleUser(Resource):
 
     def delete(self, user_id):
         params = request.args
-        UserModel.delete(user_id)
-        return {'deleted': 'true'}
+        usr = UserModel.get(str(user_id))
+        usr.delete()
+        return {}
     
-    def get(self, list_id):
-        lm = ListModel.query(list_id)
-        # serialize lm
-        return lm
+    def get(self, user_id):
+        usr = UserModel.get(str(user_id))
+        return usr.to_dict()
     
