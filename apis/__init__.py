d3bda69edd61e8a6ea8fed7376661e540969505a
David Jarzombek <davidjarzombek@Davids-MacBook-Pro.local>
2020-10-11 19:07:09 -0400

    update

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,7 +2,6 @@ from flask_restx import Api
 
 from .lists import api as list_namespace
 from .user import api as user_namespace
-from .auth import api as auth_namespace
 # etc etc.
 
 api = Api(
@@ -11,4 +10,3 @@ api = Api(
 
 api.add_namespace(list_namespace)
 api.add_namespace(user_namespace)
-api.add_namespace(auth_namespace)
